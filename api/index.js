057e320d20ab10db703afae408389a8c1bbf0d1b
jegutierrez <jesusgut24@gmail.com>
2016-06-24 20:18:28 -0300

    Config

--- api/index.js
+++ api/index.js
@@ -9,6 +9,8 @@ module.exports = {
 
     router.get('/:id/comments', findComments);
 
+    router.post('/:id', insertComment(data));
+
     return router;
 
     function findAll(req, res){
@@ -23,6 +25,10 @@ module.exports = {
 
     function findComments(req, res){
       // Complete this function
+      res.json({ message: 'Your response goes here!' })
+    }
+
+    function insertComment(req, res){
       res.json({ message: 'Your response goes here!' })
     }
   }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+
 const PORT = 1338;
 
 const Datastore = require('nedb');
