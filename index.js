d0d3ffa36e76c6bc84626ceaadfb49fb370e5277
Rodrigo Garcia <mysery@gmail.com>
2016-06-25 17:50:36 -0300

    descomento service routes

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/bands', api.bandsRoutes(db, commentsSrv));
-// app.use('/albums', api.albumsRoutes(db, commentsSrv));
-// app.use('/tracks', api.tracksRoutes(db, commentsSrv));
-// app.use('/artists', api.artistsRoutes(db, commentsSrv));
-// app.use('/comments', api.commentsRoutes(commentsSrv));
+app.use('/albums', api.albumsRoutes(db, commentsSrv));
+app.use('/tracks', api.tracksRoutes(db, commentsSrv));
+app.use('/artists', api.artistsRoutes(db, commentsSrv));
+app.use('/comments', api.commentsRoutes(commentsSrv));
 
 app.listen(PORT, () => console.log(`App started and listening on port ${PORT}`));
